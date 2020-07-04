adabe296ffbdb19f53024d3c3eeb0d8b7c5206d3
Royer G <royjuni3431@gmail.com>
2020-07-04 13:38:41 -0500

    Add messages crud

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from app.messages import MessageManager
+
 def bot_telegram():
-    pass
+    bot = MessageManager('app/message.json')
+    print(bot.get_messages())
+    message = {'other message': 'test'}
+    bot.add_message(message)
+    print(bot.update_message('other message', 'Update message'))
+    
 
 if __name__ == "__main__":
     bot_telegram()
